fb32287ac3f5e471ff54c11dfb0f1d84a8edfbd4
andreasschollenberger <andreasschollenberger@gmail.com>
2024-04-23 11:47:14 +0200

    Neuer Laravel/Vue Starter

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Startseite</h1>
+        <router-link to="/login">Link zur Login Seite</router-link>
+    </div>
+</template>
+<style lang="scss">
+h1 {
+    color: red;
+}
+</style>
