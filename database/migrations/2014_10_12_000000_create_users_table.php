6bb5027fef7eb7a738ada75ec1d096ac1de4de15
Andreas Schollenberger <andreasschollenberger@gmail.com>
2024-06-10 15:00:01 +0200

    "Datenbank zum teil erstellt, CreateRezept weitere CSS anpassungen und an komponenten gearbeitet"

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->enum('type', ['admin', 'mod', 'user'])->default('user');
+            // $table->string('name'); 
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('agb_confirmed')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/js/pages/CreateRezept.vue
+++ resources/js/pages/CreateRezept.vue
@@ -83,34 +83,44 @@
         <div>
         <h1>Zutaten erstellen</h1>
         <div class="input-container">
+
+            <div class="infobox">
+                <p class="i-titel">Zutaten erstellen:</p>
+                <p>Die Zutaten werden in der Reihenfolge angezeigt, in der sie hinzugefügt wurden.
+                    Zuerst wird die Menge wie z.B. 2, dann die Einheit wie 
+                    z.B. EL (EL = Esslöffel) und zuletzt die Zutat wie z.B. Mehl angezeigt.</p>
+                <p>Eine neue Zutat können durch Klicken auf das Pluszeichen hinzugefühgt werden.</p>
+                <p>EL = Esslöffel, Kg. = Kilogramm, g = Gramm, Stk. = Stück l = Liter, Dl. = Deziliter.  </p>
+            </div>
+
+
             <input class="nr-field" type="number" placeholder="Einheit Eingeben (Zahl)" v-model="numberValue" />
             <select class="el-drop" v-model="unitValue">
                 <option value="EL">EL</option>
+                <option value="kg">Kg.</option>
                 <option value="g">g</option>
                 <option value="stk">Stk.</option>
+                <option value="l">l.</option>
+                <option value="dl">Dl.</option>
             </select>
             <textarea class="textfield" placeholder="Zutat Eingeben" v-model="textValue"></textarea>
             <button class="plus-b" @click="addFields">+</button>
-        </div>
-        <div class="input-container" v-for="(field, index) in fields" :key="index">
-            <input class="nr-field" type="number" placeholder="Einheit Eingeben (Zahl)" v-model="field.numberValue" />
-            <select class="el-drop" v-model="field.unitValue">
-                <option value="EL">EL</option>
-                <option value="g">g</option>
-                <option value="stk">Stk.</option>
-            </select>
-            <textarea class="textfield" placeholder="Zutat Eingeben" v-model="field.textValue"></textarea>
+        <!-- </div> -->
+            <div class="input-container2" v-for="(field, index) in fields" :key="index">
+                <input class="nr-field" type="number" placeholder="Einheit Eingeben (Zahl)" v-model="field.numberValue" />
+                <select class="el-drop" v-model="field.unitValue">
+                    <option value="EL">EL</option>
+                    <option value="kg">Kg.</option>
+                    <option value="g">g</option>
+                    <option value="stk">Stk.</option>
+                    <option value="l">l.</option>
+                    <option value="dl">Dl.</option>
+                </select>
+                <textarea class="textfield" placeholder="Zutat Eingeben" v-model="field.textValue"></textarea>
+            </div>
         </div>
     </div>
 
-    <div class="infobox">
-        <p class="i-titel">Zutaten erstellen:</p>
-        <p>Die Zutaten werden in der Reihenfolge angezeigt, in der sie hinzugefügt wurden.
-            Zuerst wird die Menge wie z.B. 2, dann die Einheit wie 
-            z.B. EL (EL = Esslöffel) und zuletzt die Zutat wie z.B. Mehl angezeigt.</p>
-        <p>Eine neue Zutat können durch Klicken auf das Pluszeichen hinzugefühgt werden.</p>
-        <p>EL = Esslöffel, g = Gramm, Stk. = Stück. </p>
-    </div>
         
         <div class="zu-container">
             <div class="zubereitung">
@@ -292,12 +302,89 @@ main {
     line-height: 28px;
 }
 
+/* Zutaten Container */
+
+.input-container {
+    display: flex;
+}
+
+.input-container2 {
+    display: flex;
+    /* flex-direction: column; */
+}
+
+
+.nr-field {
+    width: 200px;
+    height: 30px;
+    margin-right: 20px;
+    margin-bottom: 20px;
+    font-size: 16px;
+    text-align: center;
+    line-height: 28px;
+}
+
+.el-drop {
+    width: 50px;
+    height: 30px;
+    margin-right: 20px;
+    margin-bottom: 20px;
+    font-size: 16px;
+    text-align: center;
+    line-height: 28px;
+}
+
+.textfield {
+    width: 300px;
+    height: 30px;
+    margin-right: 20px;
+    margin-bottom: 20px;
+    font-size: 16px;
+    text-align: center;
+    line-height: 28px;
+}
+
+.plus-b {
+    width: 50px;
+    height: 50px;
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    border-radius: 50%;
+    cursor: pointer;
+    /* position: absolute;
+    left: 630px; */
+    font-size: 40px;
+    text-align: center; 
+    line-height: 28px;
+}
+
+.infobox {
+    width: 300px;
+    background-color: rgba(179, 187, 195, 1);
+    border-radius: 10px;
+    padding: 20px;
+    margin: 0 10px 0 50px ;
+    font-size: 18px;
+
+    /* position : absolute;
+    top: 0px;
+    left: 700px; */
+}
+
+.i-titel {
+    font-size: 20px;
+    font-weight: bold;
+    color: #393636;
+    margin-bottom: 10px;
+}
+
+
+/* Zubereitung Container */
 .zu-container {
     display: flex;
     width: 500px;
     margin: 50px 0 0 400px;
-    /* margin-left: 100px; */
-    /* padding: 20px; */
 }
 
 .textfield {
@@ -322,4 +409,6 @@ main {
     text-align: center; 
     line-height: 28px;
 }
+
+
 </style>
